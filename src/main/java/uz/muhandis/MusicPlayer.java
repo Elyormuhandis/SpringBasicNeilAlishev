9c0cd7ef3b99a9777aa28dd9e4999179e78c3a06
elyormuhandis <elyormuhandis@gmail.com>
2023-04-24 16:15:31 +0500

    give musicList to MusicPlayer property by xml tag

--- src/main/java/uz/muhandis/MusicPlayer.java
+++ src/main/java/uz/muhandis/MusicPlayer.java
@@ -1,23 +1,28 @@
 package uz.muhandis;
 
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList;
     private String name;
     private int volume;
 
 
     public MusicPlayer(){}
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic(){
-        System.out.println("Playing " + music.getMusic());
+        for (Music music:musicList
+             ) {
+            System.out.println("Playing " + music.getMusic());
+        };
     }
 
     public String getName() {
--- src/main/java/uz/muhandis/SpringApp.java
+++ src/main/java/uz/muhandis/SpringApp.java
@@ -6,11 +6,7 @@ public class SpringApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayerBean = context.getBean("musicPlayerBean", MusicPlayer.class);
-
-        System.out.println("Music player " + musicPlayerBean.getName() + " volume " + musicPlayerBean.getVolume());
         musicPlayerBean.playMusic();
-
-
         context.close();
     }
 }
